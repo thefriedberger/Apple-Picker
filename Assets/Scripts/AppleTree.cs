5b8e0c7853585d2e604af3aa7531e8610446a4fb
Tyler Friedberg <thefriedberger@gmail.com>
2017-09-15 15:25:39 -0400

    Initial Commit

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    //Prefab for instantiating apples
+    public float applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirection = 0.1f;
+    public float secondsBetweenDrops = 1f;
+
+	// Use this for initialization
+	void Start () {
+        //dropping apples every second
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //changing direction
+        if (pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        } else if (Random.value < chanceToChangeDirection) {
+            speed *= -1;
+        }
+    }
+    void FixedUpdate()
+    {
+        
+    }
+}
