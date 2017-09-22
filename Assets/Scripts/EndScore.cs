6f93a4a278465f6bf1e6ed7d2372f410f07f5d38
Tyler Friedberg <thefriedberger@gmail.com>
2017-09-22 12:35:15 -0400

    Fixed EndScore
    Fixed endscore so it displays 0 if score is 0.

--- Assets/Scripts/EndScore.cs
+++ Assets/Scripts/EndScore.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class EndScore : MonoBehaviour {
-    static public int score = 1000;
+    static public int score = 0;
 
     // Update is called once per frame
     void Update() {
